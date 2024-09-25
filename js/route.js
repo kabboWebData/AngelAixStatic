64d79dd1d1f11b42ce74117f81eaf5ac27118f7b
Essential-Infotech <137377088+kabboWebData@users.noreply.github.com>
2024-09-25 13:26:06 +0600

    Angel Aix
    For client Feedback

---
+++ js/route.js
@@ -0,0 +1,4 @@
+document.getElementById('nav-button').addEventListener('click', function() {
+    // Code to execute when the button is clicked
+    console.log('Nav button clicked!');
+});
---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('navigation-button').addEventListener('click', function() {
+    const nav = document.getElementById('mobile-nav');
+    nav.classList.toggle('hidden'); // Toggles the visibility of the navigation menu
+});
